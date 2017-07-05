743ccc0948ede4e0c6bd160d63de4d1dc740b51c
Max Paulus <max_paulus@yahoo.de>
2017-07-05 23:18:09 +0200

    seq-model

--- helpers/seq-model.py
+++ helpers/seq-model.py
@@ -1,6 +1,6 @@
 class seq_model.py(object):
 
-	def __init__(self, seq_length, n_labels, A, Q, Q1, G):
+	def __init__(self, seq_length, n_labels, sim_dim, k):
 		"""
 		A = N x N sparse edge potential matrix; A(i,j) is potential from label i to j
 		Q = 1 x N node potential vector (applies to all except (possibly) first node)
@@ -10,9 +10,17 @@ class seq_model.py(object):
 		"""
 
 		self.seq_length = seq_length # Alex calls it T
-		self.n_labels = n_labels # Alex calls it N
-		self.A = # 
+		self.n_labels = n_labels # Alex calls it np
+		self.sim_dim = sim_dim
+		self.k = k
+
+		temp = np.tile(np.arange(seq_length) / seq_length, (seq_length,1))
+		self.A = np.exp(-1e4 * (temp - temp.T) **2)
 		self.Q = np.ones(N)
-		self.Q1 = np.exp(-1000*((np.arange(100) / N) - 0.5)**2)
-		self.G = 
+		self.Q1 = np.exp(-1000*((np.arange(self.seq_length) / self.seq_length) - 0.5) **2)
+		temp = np.tile(np.arange(seq_length) / seq_length, (sim_dim, 1))
+		temp2 = np.tile(np.arange(sim_dim) / sim_dim, (seq_length, 1))
+		self.G = np.exp(-1(temp.T - temp2) ** 2)
+
+		
 
